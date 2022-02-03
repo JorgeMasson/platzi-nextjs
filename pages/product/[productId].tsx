0a25b6109d269411ddbdb8475da73c98ff6e0394
JorgeMasson <j_masson_reynoso@hotmail.com>
2022-02-02 19:27:28 -0700

    ds

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 const ProductItem = () => {
+    const { query: { productId } } = useRouter();
     return (
         <div>
-            Esta es la página del producto
+            Esta es la página del producto {productId}
         </div>
     )
 }
